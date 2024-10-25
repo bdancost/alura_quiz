702d7bb85b71cb007815308a03fcc60a45651ecc
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2024-10-24 22:07:09 -0300

    Adicionando novas funcionalidade no arquivo page.tsx

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -2,10 +2,22 @@ import { AluraQuizLogo } from "../_components/AluraquizLogo";
 import { Card } from "../_components/Card";
 import { Footer } from "../_components/Footer";
 import pageStyles from "../page.module.css";
+import config from "../../config.json";
+
+const questions = config.questions;
 
 export default function GameScreen() {
+  const currentQuestion = 0;
+  const questionNumber = currentQuestion + 1;
+  const question = questions[currentQuestion];
   return (
-    <main className={pageStyles.screen} style={{ flex: 1 }}>
+    <main
+      className={pageStyles.screen}
+      style={{
+        flex: 1,
+        backgroundImage: `url("${question.image}")`,
+      }}
+    >
       <section className={pageStyles.container}>
         <div
           style={{
@@ -16,9 +28,9 @@ export default function GameScreen() {
         >
           <AluraQuizLogo />
         </div>
-        <Card headerTitle="Pergunta 1 de 5">
-          <h1>Quando foi lançado o primeiro filme do homem de ferro</h1>
-          <p>Considere somente os filmes novos da Marvel</p>
+        <Card headerTitle={`Pergunta ${questionNumber} de ${questions.length}`}>
+          <h1> {question.title} </h1>
+          <p> {question.description} </p>
         </Card>
         <Footer />
       </section>
